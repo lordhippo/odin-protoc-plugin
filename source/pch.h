68d787ba052edfac0423e8bdc3366deaa8abcdc4
Ali Salehi <nysalehi@gmail.com>
2024-02-24 16:05:21 +0100

    Add auto-generated notice.

--- source/pch.h
+++ source/pch.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/io/printer.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/compiler/plugin.pb.h>
 #include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/printer.h>
+
+#include <algorithm>
+#include <format>
+#include <vector>
