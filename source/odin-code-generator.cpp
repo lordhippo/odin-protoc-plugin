91a938ab6d1cfd6b4da3c8c5cd157e12f659cbd7
Ali Salehi <nysalehi@gmail.com>
2024-02-24 14:21:07 +0100

    Finished implementing the basic parser.

--- source/odin-code-generator.cpp
+++ source/odin-code-generator.cpp
@@ -1,46 +1,319 @@
 #include "odin-code-generator.h"
 
-bool OdinCodeGenerator::Generate(
-    const google::protobuf::FileDescriptor *const file,
-    const std::string &parameter,
-    google::protobuf::compiler::GeneratorContext *const generator_context,
-    std::string *const error) const
+#include <algorithm>
+#include <vector>
+
+using namespace google::protobuf;
+
+struct Context
 {
-    auto* output = generator_context->Open(file->name() + ".odin");
+    google::protobuf::io::Printer printer;
+    std::string *error;
+};
 
-    google::protobuf::io::Printer printer(output, '$');
+// replaces '.' with '_' in type names.
+// idx.e. FirstType.SecondType -> FirstType_SecondType
+static std::string ConvertFullTypeName(const std::string_view full_name)
+{
+    std::string result{full_name};
+    std::replace(result.begin(), result.end(), '.', '_');
+    return result;
+}
 
-    printer.Print("package protos\n\n");
+enum class OdinBuiltinType
+{
+    t_none = 0,
+    t_i32 = 1,         // TYPE_INT32, TYPE_SINT32, TYPE_SFIXED32
+    t_i64 = 2,         // TYPE_INT64, TYPE_SINT64, TYPE_SFIXED64
+    t_u32 = 3,         // TYPE_UINT32, TYPE_FIXED32
+    t_u64 = 4,         // TYPE_UINT64, TYPE_FIXED64
+    t_f64 = 5,         // TYPE_DOUBLE
+    t_f32 = 6,         // TYPE_FLOAT
+    t_bool = 7,        // TYPE_BOOL
+    t_enum = 8,        // TYPE_ENUM
+    t_string = 9,      // TYPE_STRING
+    t_byte_slice = 10, // TYPE_BYTES
+    t_message = 11,    // TYPE_MESSAGE, TYPE_GROUP
+};
 
-    for (int i = 0; i < file->message_type_count(); ++i)
+static OdinBuiltinType GetOdinBuiltinType(const FieldDescriptor::Type type)
+{
+    switch (type)
     {
-        GenerateMessage(file->message_type(i), &printer);
+    case FieldDescriptor::TYPE_DOUBLE:
+        return OdinBuiltinType::t_f64;
+    case FieldDescriptor::TYPE_FLOAT:
+        return OdinBuiltinType::t_f32;
+    case FieldDescriptor::TYPE_INT64:
+        return OdinBuiltinType::t_i64;
+    case FieldDescriptor::TYPE_UINT64:
+        return OdinBuiltinType::t_u64;
+    case FieldDescriptor::TYPE_INT32:
+        return OdinBuiltinType::t_i32;
+    case FieldDescriptor::TYPE_FIXED64:
+        return OdinBuiltinType::t_u64;
+    case FieldDescriptor::TYPE_FIXED32:
+        return OdinBuiltinType::t_u32;
+    case FieldDescriptor::TYPE_BOOL:
+        return OdinBuiltinType::t_bool;
+    case FieldDescriptor::TYPE_STRING:
+        return OdinBuiltinType::t_string;
+    case FieldDescriptor::TYPE_GROUP:
+        return OdinBuiltinType::t_message;
+    case FieldDescriptor::TYPE_MESSAGE:
+        return OdinBuiltinType::t_message;
+    case FieldDescriptor::TYPE_BYTES:
+        return OdinBuiltinType::t_byte_slice;
+    case FieldDescriptor::TYPE_UINT32:
+        return OdinBuiltinType::t_u32;
+    case FieldDescriptor::TYPE_ENUM:
+        return OdinBuiltinType::t_enum;
+    case FieldDescriptor::TYPE_SFIXED32:
+        return OdinBuiltinType::t_i32;
+    case FieldDescriptor::TYPE_SFIXED64:
+        return OdinBuiltinType::t_i64;
+    case FieldDescriptor::TYPE_SINT32:
+        return OdinBuiltinType::t_i32;
+    case FieldDescriptor::TYPE_SINT64:
+        return OdinBuiltinType::t_i64;
+    default:
+        return OdinBuiltinType::t_none;
     }
+}
+
+static std::string_view GetOdinBuiltinTypeName(const OdinBuiltinType type)
+{
+    switch (type)
+    {
+    case OdinBuiltinType::t_none:
+        return "";
+    case OdinBuiltinType::t_i32:
+        return "i32";
+    case OdinBuiltinType::t_i64:
+        return "i64";
+    case OdinBuiltinType::t_u32:
+        return "u32";
+    case OdinBuiltinType::t_u64:
+        return "u64";
+    case OdinBuiltinType::t_f64:
+        return "f64";
+    case OdinBuiltinType::t_f32:
+        return "f32";
+    case OdinBuiltinType::t_bool:
+        return "bool";
+    case OdinBuiltinType::t_enum:
+        return "";
+    case OdinBuiltinType::t_string:
+        return "string";
+    case OdinBuiltinType::t_byte_slice:
+        return "[]u8";
+    case OdinBuiltinType::t_message:
+        return "";
+    default:
+        return "";
+    }
+}
+
+static std::string GetOdinFieldTypeName(const FieldDescriptor &field_desc)
+{
+    std::string type_name{};
+
+    if (const Descriptor *const message_desc = field_desc.message_type(); message_desc)
+    {
+        type_name = ConvertFullTypeName(message_desc->full_name());
+    }
+    else if (const EnumDescriptor *const enum_desc = field_desc.enum_type(); enum_desc)
+    {
+        type_name = ConvertFullTypeName(enum_desc->full_name());
+    }
+    else
+    {
+        const OdinBuiltinType odin_type = GetOdinBuiltinType(field_desc.type());
+        type_name = std::string{GetOdinBuiltinTypeName(odin_type)};
+    }
+
+    if (field_desc.is_repeated())
+    {
+        type_name = "[](" + type_name + ")";
+    }
+
+    return type_name;
+}
+
+static std::string GetFieldTags(const FieldDescriptor &field_desc)
+{
+    std::string result{};
+
+    if (field_desc.is_packed())
+    {
+        result += " packed";
+    }
+
+    return result;
+}
+
+static bool PrintField(const FieldDescriptor &field_desc, Context *const context)
+{
+    // TODO: handle maps
+    // TODO: handle default values
+
+    const std::map<std::string, std::string> vars{
+        {"name", field_desc.name()},
+        {"odin_type", GetOdinFieldTypeName(field_desc)},
+        {"id", std::to_string(field_desc.number())},
+        {"proto_type", std::to_string((int) field_desc.type())},
+        {"tags", GetFieldTags(field_desc)},
+    };
+
+    context->printer.Print(vars, "$name$ : $odin_type$ `id:\"$id$\" type:\"$proto_type$\"$tags$`\n");
+
     return true;
 }
 
-void OdinCodeGenerator::GenerateMessage(
-    const google::protobuf::Descriptor *const message,
-    google::protobuf::io::Printer *const      printer) const
+static bool PrintOneof(const OneofDescriptor &oneof_desc, Context *const context)
 {
-    printer->Print("$name$ :: struct {\n", "name", message->name());
-    printer->Indent();
+    std::map<std::string, std::string> vars{
+        {"name", oneof_desc.name()},
+    };
 
-    for (int i = 0; i < message->field_count(); i++)
+    context->printer.Print(vars, "$name$: struct #raw_union {\n");
+    context->printer.Indent();
+
+    for (int idx = 0; idx < oneof_desc.field_count(); ++idx)
     {
-        auto *field = message->field(i);
-        printer->Print("$name$ : $type$\n", 
-            "name", field->name(), 
-            "type", field->type_name());
+        if (!PrintField(*oneof_desc.field(idx), context))
+        {
+            return false;
+        }
     }
 
-    // TODO: add fields
+    context->printer.Outdent();
+    context->printer.Print("}\n");
 
-    printer->Outdent();
-    printer->Print("}\n");
+    return true;
+}
 
-    for (int i = 0; i < message->nested_type_count(); ++i)
+static bool PrintEnum(const EnumDescriptor &enum_desc, Context *const context)
+{
+    std::map<std::string, std::string> vars{
+        {"name", ConvertFullTypeName(enum_desc.full_name())},
+    };
+
+    context->printer.Print(vars, "$name$ :: enum {\n");
+    context->printer.Indent();
+
+    vars.clear();
+
+    for (int idx = 0; idx < enum_desc.value_count(); ++idx)
     {
-        GenerateMessage(message->nested_type(i), printer);
+        const EnumValueDescriptor &value_desc = *enum_desc.value(idx);
+
+        vars["name"] = value_desc.name();
+        vars["value"] = std::to_string(value_desc.number());
+
+        context->printer.Print(vars, "$name$ = $value$,\n");
     }
+
+    context->printer.Outdent();
+    context->printer.Print("}\n");
+
+    return true;
+}
+
+static bool PrintMessage(const Descriptor &message_desc, Context *const context)
+{
+    const std::map<std::string, std::string> vars{
+        {"name", ConvertFullTypeName(message_desc.full_name())},
+    };
+
+    context->printer.Print(vars, "$name$ :: struct {\n");
+    context->printer.Indent();
+
+    for (int idx = 0; idx < message_desc.field_count(); ++idx)
+    {
+        if (!PrintField(*message_desc.field(idx), context))
+        {
+            return false;
+        }
+    }
+
+    context->printer.Outdent();
+    context->printer.Print("}\n");
+
+    for (int idx = 0; idx < message_desc.nested_type_count(); ++idx)
+    {
+        if (!PrintMessage(*message_desc.nested_type(idx), context))
+        {
+            return false;
+        }
+    }
+
+    for (int idx = 0; idx < message_desc.enum_type_count(); ++idx)
+    {
+        if (!PrintEnum(*message_desc.enum_type(idx), context))
+        {
+            return false;
+        }
+    }
+
+    for (int idx = 0; idx < message_desc.oneof_decl_count(); ++idx)
+    {
+        if (!PrintOneof(*message_desc.oneof_decl(idx), context))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static bool PrintFile(const FileDescriptor &file_desc, Context *const context)
+{
+    std::string package_name = "proto";
+    if (!file_desc.package().empty())
+    {
+        package_name += '_';
+        package_name += file_desc.package();
+    }
+
+    const std::map<std::string, std::string> vars{
+        {"package", package_name},
+    };
+
+    context->printer.Print(vars, "package $package$\n\n");
+
+    // TODO: handle dependencies, i.e. file_desc.dependency and file_desc.public_dependency
+
+    for (int idx = 0; idx < file_desc.message_type_count(); ++idx)
+    {
+        if (!PrintMessage(*file_desc.message_type(idx), context))
+        {
+            return false;
+        }
+    }
+
+    for (int idx = 0; idx < file_desc.enum_type_count(); ++idx)
+    {
+        if (!PrintEnum(*file_desc.enum_type(idx), context))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool OdinCodeGenerator::Generate(const FileDescriptor *const file, const std::string &parameter,
+                                 compiler::GeneratorContext *const generator_context, std::string *const error) const
+{
+    const std::string output_filename = compiler::StripProto(file->name()) + ".pb.odin";
+    auto *output = generator_context->Open(output_filename);
+    io::Printer printer(output, '$');
+
+    Context context{
+        .printer = {output, '$'},
+        .error = error,
+    };
+
+    return PrintFile(*file, &context);
 }
--- source/odin-code-generator.h
+++ source/odin-code-generator.h
@@ -3,12 +3,6 @@
 class OdinCodeGenerator final : public google::protobuf::compiler::CodeGenerator
 {
 public:
-    bool Generate(
-        const google::protobuf::FileDescriptor *file,
-        const std::string &parameter,
-        google::protobuf::compiler::GeneratorContext *generator_context,
-        std::string *error) const override;
-
-private:
-    void GenerateMessage(const google::protobuf::Descriptor *message, google::protobuf::io::Printer *printer) const;
+    bool Generate(const google::protobuf::FileDescriptor *file, const std::string &parameter,
+                  google::protobuf::compiler::GeneratorContext *generator_context, std::string *error) const override;
 };
